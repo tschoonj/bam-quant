5a73e8d6fe15c9a8c92c105949d46b54ac16bc66
Tom Schoonjans <Tom.Schoonjans@me.com>
2014-10-19 16:39:18 +0200

    BAM::ASR::Parse now switches the linetype to the xraylib counterpart
    plus some additional exception handling

--- src/bam_file_asr.cpp
+++ src/bam_file_asr.cpp
@@ -2,22 +2,23 @@
 #include "bam_exception.h"
 #include <iostream>
 #include <sstream>
+#include <xraylib.h>
 
 using namespace BAM;
 using namespace BAM::File;
 using namespace std;
 
 ASR::ASR(string filename) : File::File(filename) {
-	cout << "Entering BAM::FileASR constructor" << endl;
+	cout << "Entering BAM::File::ASR constructor" << endl;
 	if (fs) {
 		this->Parse();
 	}
 	this->Close();
-	cout << "Leaving BAM::FileASR constructor" << endl;
+	cout << "Leaving BAM::File::ASR constructor" << endl;
 }
 
 ASR::~ASR() {
-	cout << "Entering BAM::FileASR destructor" << endl;
+	cout << "Entering BAM::File::ASR destructor" << endl;
 
 }
 
@@ -28,54 +29,68 @@ void ASR::Parse() {
 	bool peaks_found = false;
 	bool normfactor_found = false;
 
- 	try {	
-		while (getline(fs, line)) {
-			//cout << line << endl;
-			if (!peaks_found) {
-				//process metadata
-				stringstream ss;
-				string identifier;
+	while (1) {
+ 		try {	
+			getline(fs, line);
+		}
+		catch (...) {
+			break;
+		}
+		//cout << line << endl;
+		if (!peaks_found) {
+			//process metadata
+			stringstream ss;
+			string identifier;
 
-				ss << line;
-				ss >> identifier;
-				if (identifier.compare(0, 7, "$PEAKS:") == 0) {
-					peaks_found = true;
-					cout << "peaks found!" << endl;
-					//basically we ignore the line with the number of peaks
-					getline(fs,line);
-					continue;
-				}
-				else if (identifier == "Normfactor:") {
-					ss >> normfactor;
-					normfactor_found = true;
-					cout << "Normfactor: " << normfactor << endl;
-				}
+			ss << line;
+			ss >> identifier;
+			if (identifier.compare(0, 7, "$PEAKS:") == 0) {
+				peaks_found = true;
+				cout << "peaks found!" << endl;
+				//basically we ignore the line with the number of peaks
+				getline(fs,line);
+				continue;
 			}
-			else {
-				int Z;
-				int elem_line;
-				double counts;
-				double stddev;
-				double chi;
-				double bg;
-			
-				stringstream ss;
-				ss << line;
-				ss >> Z >> elem_line >> counts >> stddev >> chi >> bg;
-				data_asr.push_back(Data::ASR(Z, elem_line, counts, stddev, chi, bg));
+			else if (identifier == "Normfactor:") {
+				ss >> normfactor;
+				normfactor_found = true;
+				cout << "Normfactor: " << normfactor << endl;
 			}
 		}
+		else {
+			int Z;
+			int elem_line;
+			double counts;
+			double stddev;
+			double chi;
+			double bg;
+		
+			stringstream ss;
+			ss << line;
+			ss >> Z >> elem_line >> counts >> stddev >> chi >> bg;
+			//switch to xraylib's lines
+			
+			if (elem_line == 1) {
+				elem_line = KA_LINE;
+			}
+			else if (elem_line == 2) {
+				elem_line = LA_LINE;
+			}
+			else {
+				throw BAM::Exception("BAM::ASR::Parse -> Invalid line identifier found in "+filename+": "+ static_cast<ostringstream*>( &(ostringstream() << elem_line) )->str());
+			}
+			data_asr.push_back(Data::ASR(Z, elem_line, counts, stddev, chi, bg));
+		}
 	}
-	catch (...) {}
 
 	if (!normfactor_found) {
-		throw Exception("Normfactor not found in "+filename);
+		throw BAM::Exception("BAM::ASR::Parse ->Normfactor not found in "+filename);
 	}
 	else if (!peaks_found) {
-		throw Exception("No $PEAKS: found in "+filename);
+		throw BAM::Exception("BAM::ASR::Parse -> No $PEAKS: found in "+filename);
 	}
 	else if (data_asr.size() == 0) {
-		throw Exception("No peaks found in "+filename);
+		throw BAM::Exception("BAM::ASR::Parse -> No peaks found in "+filename);
 	}
 
 	cout << "Leaving BAM::FileASR Parse" << endl;
